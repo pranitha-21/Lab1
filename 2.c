b1d43261d73e2aedbdbf13eadafa35345ed0f74b
pranitha-21 <pranithapranitha388@gmail.com>
2024-09-04 20:58:01 +0530

    2.c

--- 2.c
+++ 2.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void main()
 {
-    int a,b,c,d,e,sum;
+    int subject1,subject2,subject3,subject4,subject5,sum;
     printf("enter the values of a,b,c,d,e");
     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
     sum=a+b+c+d+e;
-    printf("sum=%d",sum);
+    printf("marks scored=%d",sum);
     float percentage;
     int totalmarks=500;
     int scored=sum;
